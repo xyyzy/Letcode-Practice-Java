539cbf1bcfd68a5ca2d175d476bae61c7f5553a0
xujianwei <623707810@qq.com>
2020-03-03 13:42:32 +0800

    第二次提交增加二叉树层序遍历

---
+++ src/树/_102_二叉树层序遍历/Solution.java
@@ -0,0 +1,33 @@
+package 树._102_二叉树层序遍历;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    List<List<Integer>> list = new ArrayList<List<Integer>>();
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root==null)
+            return list;
+        int level =0;
+        helper(root,level);
+        return  list;
+    }
+    private void helper(TreeNode node,int level){
+        if (list.size()==level)
+            list.add(new ArrayList<Integer>());
+        list.get(level).add(node.val);
+        if (node.left!=null)
+            helper(node.left,level+1);
+        if (node.right!=null)
+            helper(node.right,level+1);
+    }
+}
