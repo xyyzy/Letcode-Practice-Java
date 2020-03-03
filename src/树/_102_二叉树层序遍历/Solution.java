02a923f1c017a10b06a994272989e12f537122fc
xujianwei <623707810@qq.com>
2020-03-03 13:46:30 +0800

    第二次提交增加二叉树层序遍历

--- src/树/_102_二叉树层序遍历/Solution.java
+++ src/树/_102_二叉树层序遍历/Solution.java
@@ -1,7 +1,9 @@
 package 树._102_二叉树层序遍历;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Solution {
     public class TreeNode {
@@ -13,6 +15,9 @@ public class Solution {
             val = x;
         }
     }
+    /*
+    * 递归
+    * */
     List<List<Integer>> list = new ArrayList<List<Integer>>();
     public List<List<Integer>> levelOrder(TreeNode root) {
         if (root==null)
@@ -22,7 +27,6 @@ public class Solution {
         return  list;
     }
 
-
     private void helper(TreeNode node,int level){
         if (list.size()==level)
             list.add(new ArrayList<Integer>());
@@ -31,5 +35,30 @@ public class Solution {
             helper(node.left,level+1);
         if (node.right!=null)
             helper(node.right,level+1);
+    }
+    /*
+    * 迭代
+    * */
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        List<List<Integer>> lists = new ArrayList<List<Integer>>();
+        if (root == null) return lists;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int level = 0;
+        while (!queue.isEmpty()) {
+            lists.add(new ArrayList<Integer>());
+
+            int level_size = queue.size();
+            for (int i = 0; i <level_size; i++) {
+                TreeNode temp = queue.remove();
+                lists.get(level).add(temp.val);
+                if (temp.left!=null)
+                    queue.add(temp.left);
+                if (temp.right!=null)
+                    queue.add(temp.right);
+            }
+            level++;
+        }
+        return lists;
     }
 }
